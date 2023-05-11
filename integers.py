12e1ecde66153b8798bb87c4f3606b5bc1e3ecf8
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-11 13:43:36 +0800

    Defining details of the program

--- integers.py
+++ integers.py
@@ -1,1 +1,6 @@
-
+def details ():
+    Description = "Reading text file and creting two other text files."
+    Date = "05-11-23"
+    print ("\nDescription: {}\nDate: {}".format (Description , Date))
+    
+details ()
