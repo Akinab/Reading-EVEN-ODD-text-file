31c9a905d25c03433463f297181bf2db5ab21b77
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-11 14:09:48 +0800

    Creating two separate files containing even and odd numbers

--- integers.py
+++ integers.py
@@ -1,5 +1,5 @@
 def details ():
-    Description = "Reading text file and creting two other text files."
+    Description = "Reading text file and creating two other text files."
     Date = "05-11-23"
     print ("\nDescription: {}\nDate: {}".format (Description , Date))
     
